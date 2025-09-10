91a37acda09172b92e2af36246e7288377d87d4a
unknown <lavenderia.teknik@gmail.com>
2025-09-10 16:14:51 +0700

    test server local with https

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import fs from 'fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -17,4 +18,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',   // biar bisa diakses dari device lain
+    port: 5173,        // bisa ganti kalau bentrok
+     https: {
+      key: fs.readFileSync('./cert/vite.key'),
+      cert: fs.readFileSync('./cert/vite.crt'),
+    }
+  }
 })
