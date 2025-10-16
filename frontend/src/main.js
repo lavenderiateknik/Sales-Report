f6045151b8d583195e343577799e710229a89ef5
unknown <lavenderia.teknik@gmail.com>
2025-10-16 11:11:53 +0700

    fixing form using database

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import './assets/main.css'
 
+import "vue3-easy-data-table/dist/style.css";
+import "@/assets/easy-data-table.css";
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
